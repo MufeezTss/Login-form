6b62e8506f77449258dc851c737c7b78d8702d4e
MufeezTss <99019838+MufeezTss@users.noreply.github.com>
2022-02-25 17:54:33 +0530

    updated 25/02

--- data.js
+++ data.js
@@ -1,20 +1,10 @@
 
-window.addEventListener('load', () => {
 
-    const params = (new URL(window.location.href="http://127.0.0.1:5500/data.html?name=&email=&contact=&date=&password=")).searchParams;
-    // let uri = 'http://127.0.0.1:5500/data.html?name=&email=&contact=&date=&password=';
-    const encoded = encodeURIComponent(params) ;
-    const name = params.get('name');
-    const email = params.get('email');
-    const contact = params.get('contact');
-    const date = params.get('date');
-    const password = params.get('password');
+// const params = (new URL(window.location)).searchParams;
+// var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+// var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
 
-    document.getElementById('name').innerHTML = name;
-    document.getElementById('email').innerHTML = email;
-    document.getElementById('contact').innerHTML = contact;
-    document.getElementById('date').innerHTML = date;
-    document.getElementById('password').innerHTML = password;
-    // document.write(encoded);
-})
+// document.getElementById("myform").innerHTML = encrypted;
+// document.getElementById("myform").innerHTML = decrypted;
+
 
--- login.js
+++ login.js
@@ -1,6 +1,5 @@
-
 const myFunc = () => {
-
+  
   const  name = document.getElementById('name').value;
   const  email = document.getElementById('email').value;
   const  contact = document.getElementById('contact').value;
@@ -13,6 +12,38 @@ const myFunc = () => {
   localStorage.setItem('contact',contact);
   localStorage.setItem('date',date);
   localStorage.setItem('password',password);
-  
-}
 
+  
+};
+
+
+window.addEventListener('load', () => {
+  
+  const params = (new URL(document.location)).searchParams;
+  const name = params.get('name');
+  const email = params.get('email');
+  const contact =params.get('contact');
+  const date = params.get('date');
+  const password =params.get('password');
+  
+  document.getElementById('name').innerHTML = name;
+  document.getElementById('email').innerHTML = email;
+  document.getElementById('contact').innerHTML = contact;
+  document.getElementById('date').innerHTML = date;
+  document.getElementById('password').innerHTML = password;
+  
+  
+});
+
+
+// 
+// var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+// var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+
+// document.getElementById("myform").innerHTML = encrypted;
+// document.getElementById("myform").innerHTML = decrypted;
+
+
+// var encryptedAES = CryptoJS.AES.encrypt("Message", "My Secret Passphrase");
+// var decryptedBytes = CryptoJS.AES.decrypt(encryptedAES, "My Secret Passphrase");
+// var plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
